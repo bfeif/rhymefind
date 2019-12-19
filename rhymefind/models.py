8bcb655dca43fd004ba92679f2b9c97a84e643c9
Ben Feifke <benfeifke@gmail.com>
2019-12-19 15:51:07 -0800

    Added default values to models.

--- rhymefind/models.py
+++ rhymefind/models.py
@@ -4,25 +4,32 @@ from django.contrib.postgres.fields import ArrayField
 
 # rhyme couplet glove model
 class RhymeCouplet(models.Model):
-    word_1 = models.CharField(max_length=30)
-    word_2 = models.CharField(max_length=30)
+    
+    # fields to add
     date_added = models.DateField(auto_now_add=True)
     date_last_updated = models.DateField(auto_now=True)
-    upvotes = models.IntegerField()
-    downvotes = models.IntegerField()
-    nsfw = models.BooleanField()
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    nsfw = models.BooleanField(default=False)
+    
+    # fields from Python model
+    word_1 = models.CharField(max_length=30)
+    word_2 = models.CharField(max_length=30)
+    phoneme_seq_1 = ArrayField(models.CharField(max_length=3), null=True)
+    phoneme_seq_2 = ArrayField(models.CharField(max_length=3), null=True)
+    rhyme_seq = ArrayField(models.CharField(max_length=3), null=True)
     # glove = ArrayField(models.FloatField())
 
 
 # add glove attributes to RhymeCouplet
 for i in range(100):
-    RhymeCouplet.add_to_class('glove_' + str(i), models.FloatField(null=True))
+    RhymeCouplet.add_to_class('glove_mean_' + str(i), models.FloatField(null=True))
 
 
 # single word glove model
 class Glove(models.Model):
     word = models.CharField(max_length=30)
-    nsfw = models.BooleanField()
+    nsfw = models.BooleanField(default=False)
     # glove = ArrayField(models.FloatField())
 
 
