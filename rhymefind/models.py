fbb508c7e7f81de9a10cdaa142f08b8cb05c9ac0
Ben Feifke <benfeifke@gmail.com>
2019-12-19 17:26:07 -0800

    Added __str__() methods to models.

--- rhymefind/models.py
+++ rhymefind/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
+'''
+NOTE: DJANGO does not actually update the schema of the db when you add a default value to a column
+'''
 
 # rhyme couplet glove model
 class RhymeCouplet(models.Model):
-    
+
     # fields to add
     date_added = models.DateField(auto_now_add=True)
     date_last_updated = models.DateField(auto_now=True)
@@ -18,7 +21,10 @@ class RhymeCouplet(models.Model):
     phoneme_seq_1 = ArrayField(models.CharField(max_length=3), null=True)
     phoneme_seq_2 = ArrayField(models.CharField(max_length=3), null=True)
     rhyme_seq = ArrayField(models.CharField(max_length=3), null=True)
-    # glove = ArrayField(models.FloatField())
+
+    # method definition
+    def __str__(self):
+        return (self.word_1 + ', ' + self.word_2)
 
 
 # add glove attributes to RhymeCouplet
@@ -30,7 +36,10 @@ for i in range(100):
 class Glove(models.Model):
     word = models.CharField(max_length=30)
     nsfw = models.BooleanField(default=False)
-    # glove = ArrayField(models.FloatField())
+
+    # method definition
+    def __str__(self):
+        return (self.word)
 
 
 # add glove attributes to RhymeCouplet
