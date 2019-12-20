71c9ce35a0e1165512bae6d12aecadc7328f92c0
Ben Feifke <benfeifke@gmail.com>
2019-12-19 18:50:20 -0800

    Index view calls data from the database and shows it.

--- rhymefind/views.py
+++ rhymefind/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import RhymeCouplet, Glove
 
 
 def index(request):
-    return HttpResponse("Rhyme find, baby!")
+    gloves = Glove.objects.order_by('word')[:30]
+    template = loader.get_template('rhymefind/index.html')
+    context = {'gloves': gloves}
+    return HttpResponse(template.render(context, request))
