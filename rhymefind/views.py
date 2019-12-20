df975c45fbe746574e3d838982490f2c3c5658dc
Ben Feifke <benfeifke@gmail.com>
2019-12-20 09:18:56 -0800

    Search box to lookup word and display glove vector element.

--- rhymefind/views.py
+++ rhymefind/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import RhymeCouplet, Glove
+from django.views import View, generic
 
 
 def index(request):
-    gloves = Glove.objects.order_by('word')[:30]
-    template = loader.get_template('rhymefind/index.html')
-    context = {'gloves': gloves}
-    return HttpResponse(template.render(context, request))
+
+	words = Glove.objects.order_by('word')[:30]
+	if request.method == 'GET':
+		try:
+			search_word = request.GET.get('search_box')
+			found_glove = Glove.objects.get(word=search_word)
+			print('bandana jack')
+		except Glove.DoesNotExist:
+			found_glove = None
+	template = loader.get_template('rhymefind/index.html')
+	context = {'words': words, 'found_glove': found_glove}
+	return HttpResponse(template.render(context, request))
