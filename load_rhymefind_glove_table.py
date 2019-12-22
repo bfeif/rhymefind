b060173785615c318bf4a53c4bc688271e3c024b
Ben Feifke <benfeifke@gmail.com>
2019-12-22 09:59:51 -0800

    Adding progressbar to data loader.

--- load_rhymefind_glove_table.py
+++ load_rhymefind_glove_table.py
@@ -15,17 +15,16 @@ application = get_wsgi_application()
 # load the model
 from rhymefind.models import Glove, RhymeCouplet
 import pandas as pd
+from tqdm import tqdm
 
 # fill the rhymefind_glove table with data
 df = pd.read_pickle('/Users/benfeifke/code/RhymeFinder/data_for_site_postgres/word_glove_dictionary.pkl')
-for row_number, row in df.iterrows():
+for row_number, row in tqdm(df.iterrows()):
 	new_Glove = Glove(**row.to_dict())
 	new_Glove.save()
 
 # fill the rhymefind_rhymecouplet table with data
 df = pd.read_pickle('/Users/benfeifke/code/RhymeFinder/data_for_site_postgres/rhymecouplet_glove_dictionary.pkl')
-for row_number, row in df.iterrows():
-	print(row)
-	print(row.index)
+for row_number, row in tqdm(df.iterrows()):
 	new_RhymeCouplet = RhymeCouplet(**row.to_dict())
 	new_RhymeCouplet.save()
