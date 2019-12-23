f0e28b0b175b74deac11718e7c5a3e953cf5ed4d
Ben Feifke <benfeifke@gmail.com>
2019-12-22 23:09:46 -0800

    Updated Settings.

--- rhymefindersite/settings.py
+++ rhymefindersite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '39ji%wofwz9x7@hr1-x$2r&o!1_hes17$y@f0qa3(cgh@)y(1('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rhymefind.herokuapp.com']
 
 
 # Application definition
