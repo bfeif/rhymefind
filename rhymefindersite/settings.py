b53aead5b264513cc8d282f497ac89587525aeba
Ben Feifke <benfeifke@gmail.com>
2019-12-24 23:26:36 -0800

    Tried updating ALLOWED_HOSTS in settings.py.

--- rhymefindersite/settings.py
+++ rhymefindersite/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['rhymefind.herokuapp.com']
+ALLOWED_HOSTS = [
+    'rhymefind.herokuapp.com',
+    '.herokuapp.com',
+]
 
 
 # Application definition
