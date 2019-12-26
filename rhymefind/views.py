65c5b93b140811c0c4768674b57580d4babed1b5
Ben Feifke <benfeifke@gmail.com>
2019-12-25 16:08:43 -0800

    Added logging statements.

--- rhymefind/views.py
+++ rhymefind/views.py
@@ -1,7 +1,12 @@
+# imports
 from django.http import HttpResponse
 from django.template import loader
 from .models import RhymeCouplet, Glove
 from django.db.models import Func, F
+
+# Configure logger
+import logging
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -11,15 +16,19 @@ def index(request):
 	zipped_names = zip(couplet_glove_names, word_glove_names)
 
 	if request.method == 'GET':
+		query_word = request.GET.get('search_box')
+		logger.debug('query_word is "{}"'.format(query_word))
 		try:
-			query_word = request.GET.get('search_box')
 			found_word = Glove.objects.get(word=query_word)
+			logger.debug('found_word for rhyme couplet lookup is "{}"'.format(found_word))
 			couplets = RhymeCouplet.objects.annotate(
 				abs_diff=Func(
 					sum([(F(couplet_glove_name) - getattr(found_word, word_glove_name))**2 for couplet_glove_name, word_glove_name in zipped_names]), 
 					function='ABS'
 					)).order_by('abs_diff')[:5]
+			logger.debug('rhyme couplets found for {found_word} are {couplets}'.format(found_word=found_word, couplets=couplets))
 		except Glove.DoesNotExist:
+			logger.debug('No word found for query word "{}"'.format(query_word))
 			found_word = None
 			couplets = None
 	template = loader.get_template('rhymefind/index.html')
