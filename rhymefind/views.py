e834c148da4dddf509e2b1ed879a4e656ca0352f
Ben Feifke <benfeifke@gmail.com>
2019-12-28 22:06:43 -0800

    Styling to query code.

--- rhymefind/views.py
+++ rhymefind/views.py
@@ -21,11 +21,15 @@ def index(request):
 		try:
 			found_word = Glove.objects.get(word=query_word)
 			logger.debug('found_word for rhyme couplet lookup is "{}"'.format(found_word))
-			couplets = RhymeCouplet.objects.filter(~Q(word_1=found_word.word)).filter(~Q(word_2=found_word.word)).annotate(
-				abs_diff=Func(
-					sum([(F(couplet_glove_name) - getattr(found_word, word_glove_name))**2 for couplet_glove_name, word_glove_name in zipped_names]), 
-					function='ABS'
-					)).order_by('abs_diff')[:5]
+			couplets = RhymeCouplet.objects.\
+				filter(~Q(word_1=found_word.word)).\
+				filter(~Q(word_2=found_word.word)).\
+				annotate(
+					abs_diff=Func(
+						sum([(F(couplet_glove_name) - getattr(found_word, word_glove_name))**2 for couplet_glove_name, word_glove_name in zipped_names]), 
+						function='ABS'
+					)).\
+				order_by('abs_diff')[:5]
 			logger.debug('rhyme couplets found for {found_word} are {couplets}'.format(found_word=found_word, couplets=couplets))
 		except Glove.DoesNotExist:
 			logger.debug('No word found for query word "{}"'.format(query_word))
