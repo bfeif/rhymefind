702bdb13ca7985962e19a6f021dbdc4f25f1daeb
Ben Feifke <benfeifke.com>
2020-01-21 00:25:04 -0700

    Removing old database config from settings.py.

--- rhymefindersite/settings.py
+++ rhymefindersite/settings.py
@@ -76,13 +76,6 @@ WSGI_APPLICATION = 'rhymefindersite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.postgresql',
