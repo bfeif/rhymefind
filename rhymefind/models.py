28f60ab4804b43b842e4855fc918ced321f35bfc
Ben Feifke <benfeifke.com>
2020-01-28 16:34:59 -0500

    Created model for 50d vectors.

--- rhymefind/models.py
+++ rhymefind/models.py
@@ -5,7 +5,10 @@ from django.contrib.postgres.fields import ArrayField
 NOTE: DJANGO does not actually update the schema of the db when you add a default value to a column
 '''
 
-max_length=100
+max_length = 100
+
+################################################################
+# 100d tables
 
 # rhyme couplet glove model
 class RhymeCouplet(models.Model):
@@ -14,7 +17,7 @@ class RhymeCouplet(models.Model):
     date_added = models.DateField(auto_now_add=True)
     date_last_updated = models.DateField(auto_now=True)
     nsfw = models.BooleanField(default=False)
-    
+
     # fields from Python model
     word_1 = models.CharField(max_length=max_length)
     word_2 = models.CharField(max_length=max_length)
@@ -25,11 +28,11 @@ class RhymeCouplet(models.Model):
     # indexes
     class Meta:
         indexes = [
-            models.Index(fields=['word_1', 'word_2'], name="RhymeCouplet_word1_word2_idx"),
+            models.Index(fields=['word_1', 'word_2'],
+                         name="RhymeCouplet_word1_word2_idx"),
             models.Index(fields=['word_1'], name="RhymeCouplet_word1_idx"),
             models.Index(fields=['word_2'], name="RhymeCouplet_word2_idx")
         ]
-
 
     # method definitions
     def __str__(self):
@@ -38,12 +41,13 @@ class RhymeCouplet(models.Model):
 
 # add glove attributes to RhymeCouplet
 for i in range(100):
-    RhymeCouplet.add_to_class('glove_mean_' + str(i), models.FloatField(null=True))
+    RhymeCouplet.add_to_class('glove_mean_' + str(i),
+                              models.FloatField(null=True))
 
 
 # single word glove model
 class Glove(models.Model):
-    
+
     # fields
     word = models.CharField(max_length=max_length)
     nsfw = models.BooleanField(default=False)
@@ -62,3 +66,65 @@ class Glove(models.Model):
 # add glove attributes to RhymeCouplet
 for i in range(100):
     Glove.add_to_class('glove_' + str(i), models.FloatField(null=True))
+
+
+
+################################################################
+# 50d tables
+
+# rhyme couplet glove model
+class RhymeCouplet50d(models.Model):
+
+    # fields to add
+    date_added = models.DateField(auto_now_add=True)
+    date_last_updated = models.DateField(auto_now=True)
+    nsfw = models.BooleanField(default=False)
+
+    # fields from Python model
+    word_1 = models.CharField(max_length=max_length)
+    word_2 = models.CharField(max_length=max_length)
+    phoneme_seq_1 = ArrayField(models.CharField(max_length=3), null=True)
+    phoneme_seq_2 = ArrayField(models.CharField(max_length=3), null=True)
+    rhyme_seq = ArrayField(models.CharField(max_length=3), null=True)
+
+    # indexes
+    class Meta:
+        indexes = [
+            models.Index(fields=['word_1', 'word_2'],
+                         name="RhymeCouplet50d_word1_word2_idx"),
+            models.Index(fields=['word_1'], name="RhymeCouplet50d_word1_idx"),
+            models.Index(fields=['word_2'], name="RhymeCouplet50d_word2_idx")
+        ]
+
+    # method definitions
+    def __str__(self):
+        return (self.word_1 + ', ' + self.word_2)
+
+
+# add glove attributes to RhymeCouplet
+for i in range(50):
+    RhymeCouplet50d.add_to_class(
+        'glove_mean_' + str(i), models.FloatField(null=True))
+
+
+# single word glove model
+class Glove50d(models.Model):
+
+    # fields
+    word = models.CharField(max_length=max_length)
+    nsfw = models.BooleanField(default=False)
+
+    # indexes
+    class Meta:
+        indexes = [
+            models.Index(fields=['word'], name="Glove50d_word_idx"),
+        ]
+
+    # method definitions
+    def __str__(self):
+        return (self.word)
+
+
+# add glove attributes to RhymeCouplet
+for i in range(50):
+    Glove50d.add_to_class('glove_' + str(i), models.FloatField(null=True))
