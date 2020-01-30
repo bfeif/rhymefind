8300ad823c87169f6cbf51daf2b9145dba2a2349
Ben Feifke <benfeifke.com>
2020-01-30 02:38:43 -0500

    Script to create data for loading to Postgres tables is completed.

--- create_data_to_load_to_tables.py
+++ create_data_to_load_to_tables.py
@@ -9,14 +9,21 @@ GLOVE_LOCATION = './data/glove.6B/{}'
 GLOVE_DIMENSIONS = 50
 CMU_PRONUNCIATION_DICTIONARY_LOCATION = './'
 NUM_PARTS_TO_SAVE_AS = 1
+PATH_TO_SAVE = './data/{name}_{dimensions}d.pkl'
 
 # load the cmu dictionary to a dataframe
-rhyme_table = load_cmu_dict()
+rhyme_df = load_cmu_dict()
 
 # load glove data to dataframe
-glove_table = load_glove_dict(glove_dimensions=GLOVE_DIMENSIONS)
+glove_df = load_glove_dict(glove_dimensions=GLOVE_DIMENSIONS)
 
 # create the rhyme_couplet_glove_df
-rhyme_couplet_glove_df = create_rhyme_couplet_glove_df(glove_table, rhyme_table)
+rhyme_couplet_glove_df = create_rhyme_couplet_glove_df(glove_df, rhyme_df)
 
 # split the glove column for each table
+rhyme_couplet_glove_df = split_df_list_column(rhyme_couplet_glove_df, 'glove_mean')
+glove_df = split_df_list_column(glove_df, 'glove')
+
+# save out the files
+rhyme_couplet_glove_df.to_pickle(PATH_TO_SAVE.format(name='rhyme_couplet_glove_df', dimensions=str(GLOVE_DIMENSIONS)), protocol=4)
+glove_df.to_pickle(PATH_TO_SAVE.format(name='glove_df', dimensions=str(GLOVE_DIMENSIONS)), protocol=4)
--- data_helpers.py
+++ data_helpers.py
@@ -71,33 +71,50 @@ def create_rhyme_couplet_glove_df(glove_table, rhyme_table):
     '''
 
     # add glove data to the rhyme table
-    rhyme_glove_table = rhyme_table.merge(glove_table, how='inner', left_on='word', right_on='word')
+    rhyme_glove_table = rhyme_table.merge(
+        glove_table, how='inner', left_on='word', right_on='word')
 
     # make a copy
     rhyme_glove_table_copy = rhyme_glove_table.copy()
 
     # merge copies to make the dictionary
     couplet_dictionary = rhyme_glove_table.merge(
-        rhyme_glove_table_copy, how='inner', left_on='rhyme_seq', right_on='rhyme_seq', suffixes=('_l', '_r'))
+        rhyme_glove_table_copy, how='inner', left_on='rhyme_seq', right_on='rhyme_seq', suffixes=('_1', '_2'))
 
     # get rid of couplets of the same word, e.g. ['and', 'and']
-    couplet_dictionary = couplet_dictionary[couplet_dictionary.word_l !=
-                                            couplet_dictionary.word_r]
+    couplet_dictionary = couplet_dictionary[couplet_dictionary.word_1 !=
+                                            couplet_dictionary.word_2]
 
     # get rid of duplicates, e.g. ['and', 'band'] vs ['band', 'and']
     couplet_dictionary['word_tuple'] = couplet_dictionary.apply(
-        lambda x: tuple(sorted([x['word_l'], x['word_r']])), axis=1)
+        lambda x: tuple(sorted([x['word_1'], x['word_2']])), axis=1)
     couplet_dictionary.drop_duplicates(subset=['word_tuple'], inplace=True)
 
     # get glove_mean
-    couplet_dictionary['glove_mean'] = np.split((np.vstack(couplet_dictionary['glove_l']) + np.vstack(
-        couplet_dictionary['glove_r'])) / 2, indices_or_sections=len(couplet_dictionary), axis=0)
+    couplet_dictionary['glove_mean'] = np.split((np.vstack(couplet_dictionary['glove_1']) + np.vstack(
+        couplet_dictionary['glove_2'])) / 2, indices_or_sections=len(couplet_dictionary), axis=0)
 
     # return
     return couplet_dictionary
 
-def split_df_list_column(dataframe, column_name):
+
+def split_df_list_column(df, list_column_name):
     '''
     Takes a dataframe that has a column of type List[obj] and returns the same dataframe with new columns obj_1, obj_2, ..., obj_n
     '''
-    pass
+
+    # remove the list column name
+    columns = df.columns.to_list()
+    columns.remove(list_column_name)
+
+    # separate the df into parts
+    df_wo_list_column = df[columns]
+
+    # do the unlisting
+    unlisted_mat = np.vstack(df[list_column_name])
+    df_w_list_column_unlisted = pd.DataFrame(unlisted_mat, columns=[
+                                             list_column_name + '_{}'.format(i) for i in range(unlisted_mat.shape[1])])
+
+    # concatenate the results and return
+    new_df = pd.concat([df_wo_list_column, df_w_list_column_unlisted], axis=1)
+    return new_df
