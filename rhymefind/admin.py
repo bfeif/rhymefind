eb37effab25a72675adcfa57171e4c5b06cd9ab1
Ben Feifke <benfeifke.com>
2020-01-30 03:49:01 -0500

    Adding new lower dimensional models to admin.

--- rhymefind/admin.py
+++ rhymefind/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import RhymeCouplet, Glove
+from .models import RhymeCouplet, Glove, RhymeCouplet50d, Glove50d
 
 admin.site.register(RhymeCouplet)
 admin.site.register(Glove)
+admin.site.register(RhymeCouplet50d)
+admin.site.register(Glove50d)
