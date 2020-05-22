00421df2f9d86b927b7dfe897b11077123902777
Ben Feifke <benfeifke.com>
2020-05-22 18:07:09 -0400

    Updated view to use RhymeFind table and new Word functions.

--- rhymefind/views.py
+++ rhymefind/views.py
@@ -1,8 +1,7 @@
 # imports
 from django.http import HttpResponse
 from django.template import loader
-from .models import RhymeCouplet32dIND, Glove32dIND
-from django.db.models import Func, F, Q
+from .models import Word
 
 # Configure logger
 import logging
@@ -13,13 +12,6 @@ fun_words = ['pirate', 'paper', 'marriage', 'believe', 'feed', 'america', 'indig
 
 
 def index(request):
-
-    # constants for index view
-    couplet_glove_names = ['glove_mean_' +
-                           str(i) for i in range(GLOVE_DIMENSIONS)]
-    word_glove_names = ['glove_' + str(i) for i in range(GLOVE_DIMENSIONS)]
-    zipped_names = list(zip(couplet_glove_names, word_glove_names))
-    window_size = 3
 
     if request.method == 'GET':
 
@@ -32,43 +24,21 @@ def index(request):
         # run the query
         try:
 
-            # get the Glove32dIND object
-            found_word = Glove32dIND.objects.get(word=query_word)
+            # get the Word object
+            found_word = Word.objects.get(word=query_word)
             logger.debug(
                 'found_word for rhyme couplet lookup is "{}"'.format(found_word))
+            top_rhymefinds = list(found_word.find_rhymes().order_by('find_distance'))
 
-            # build the filter box query arguments
-            lt_filter_kwargs = {couplet_glove_name + '__gte': getattr(found_word, word_glove_name) - window_size / 2 for
-                                couplet_glove_name, word_glove_name in zipped_names}
-            gt_filter_kwargs = {couplet_glove_name + '__lte': getattr(found_word, word_glove_name) + window_size / 2 for
-                                couplet_glove_name, word_glove_name in zipped_names}
-            lt_filter_kwargs.update(gt_filter_kwargs)
-
-            # run the query
-            boxed_couplets = RhymeCouplet32dIND.objects.\
-                filter(~Q(word_1=found_word.word)).\
-                filter(~Q(word_2=found_word.word)).\
-                filter(**lt_filter_kwargs)
-            ordered_couplets = boxed_couplets.annotate(
-                abs_diff=Func(
-                    sum([(F(couplet_glove_name) - getattr(found_word, word_glove_name))
-                         ** 2 for couplet_glove_name, word_glove_name in zipped_names]),
-                    function='ABS'
-                )).\
-                order_by('abs_diff')
-            top_couplets = ordered_couplets[:10]
-            logger.debug('rhyme couplets found for {found_word} are {couplets}'.format(
-                found_word=found_word, couplets=top_couplets))
-
-        except Glove32dIND.DoesNotExist:
+        except Word.DoesNotExist:
 
             # if the Glove32dIND does not exist
             logger.debug(
                 'No word found for query word "{}"'.format(query_word))
             found_word = None
-            top_couplets = None
+            top_rhymefinds = None
 
     template = loader.get_template('rhymefind/index.html')
-    context = {'couplets': top_couplets,
+    context = {'rhymefinds': top_rhymefinds,
                'found_word': found_word, 'fun_words': fun_words}
     return HttpResponse(template.render(context, request))
