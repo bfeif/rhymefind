89d5d665db2a4a5c68638f7dcf2d3719769e6884
Ben Feifke <benfeifke.com>
2020-05-24 11:41:09 -0400

    Removed old RhymeCouplet32IND and Glove32IND models from app.

--- rhymefind/admin.py
+++ rhymefind/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RhymeCouplet32dIND, Glove32dIND
+# from .models import RhymeCouplet32dIND, Glove32dIND
 
-admin.site.register(RhymeCouplet32dIND)
-admin.site.register(Glove32dIND)
+# admin.site.register(RhymeCouplet32dIND)
+# admin.site.register(Glove32dIND)
